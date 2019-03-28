52fa61bc6d20d61b422592f4f11c33518700e54d
brajesh <brajesh.kumar@squareyards.co.in>
2019-03-28 09:35:15 +0530

    add code

---
+++ javascript/if_else.js
@@ -0,0 +1,16 @@
+var i = 10; 
+  
+if (i == 10) { 
+  
+  // First if statement 
+  if (i < 15) 
+    console.log("i is smaller than 15"); 
+  
+  // Nested - if statement 
+  // Will only be executed if statement above 
+  // it is true 
+  if (i < 12) 
+    console.log("i is smaller than 12 too"); 
+  else
+    console.log("i is greater than 15"); 
+} 
